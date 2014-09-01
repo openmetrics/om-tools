80e6af690bf1fa4e9e1ee50e0c36a86d7bf4c6af
Markus Grobelin <markus@fear-group.com>
2014-09-01 17:06:01 +0200

    fixed var substitution and added data dir

--- om-install.d/0010_install_server.sh
+++ om-install.d/0010_install_server.sh
@@ -5,6 +5,7 @@ function installServer() {
 	log "Creating directory structure...\n"
 	mkdir -p "${OM_BASE_DIR}" && chown -R ${OM_USER} "${OM_BASE_DIR}"
 	su - $OM_USER -c "mkdir -p ${OM_BASE_DIR}/config/collectd ${OM_BASE_DIR}/config/nginx"
+    su - $OM_USER -c "mkdir -p ${OM_BASE_DIR}/data ${OM_BASE_DIR}/data/collectd"
 	su - $OM_USER -c "mkdir -p ${OM_BASE_DIR}/htdocs"
 	su - $OM_USER -c "mkdir -p ${OM_BASE_DIR}/logs/nginx ${OM_BASE_DIR}/logs/collectd"
 	#su - $OM_USER -c "mkdir -p ${OM_BASE_DIR}/om-server"
@@ -29,6 +30,7 @@ function installServer() {
     # TODO make less noisy
     su - $OM_USER -c "source /etc/profile.d/rvm.sh && rvm get stable"
     su - $OM_USER -c "rvm install 2.1.1 --disable-binary && rvm use 2.1.1"
+    # TODO run gem install foreman
     su - $OM_USER -c "cd \"${OM_BASE_DIR}/om-server\" && bundle install --without development test >> /dev/null"
     if [ $? -gt 0 ] ; then
 	    log-red "Failed to execute bundle install!\n"
